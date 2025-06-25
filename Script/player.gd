8a0686c2347de4e2b9804dc7311a8879106e0795
Raimundo Acacio de Araujo Neto <raimundoneto@ce.senac.br>
2025-06-24 23:11:35 -0300

    Not much things added

--- Script/player.gd
+++ Script/player.gd
@@ -4,18 +4,13 @@ class_name Player
 @export_category("Variables")
 @export var min_spd : float = 120.0
 @export var max_spd : float = 180.0
-
-
 @onready var animations = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	moviment()
 
 func moviment():
-	var direction = Vector2.ZERO
-	var runing = Input.is_action_pressed("run")
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	var direction = Input.get_vector("left","right","up","down")
 	
 	
 	if Input.is_action_pressed("run"):
